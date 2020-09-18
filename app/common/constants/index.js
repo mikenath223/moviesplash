8bc016caf84b076dd12257fb14a4033ed21bf931
mikenath223 <michgoldennathan17@gmail.com>
2020-09-18 22:38:53 +0100

    Adjust splashScreendelay time, Add .env api_key

--- app/common/constants/index.js
+++ app/common/constants/index.js
@@ -1,3 +1,7 @@
+import MOVIES_DB_KEY from 'moviesplash/config.env.js';
+
+export const API_KEY = MOVIES_DB_KEY.API_KEY;
+
 export default {
-  SplashScreenDelayMs: 1 * 700
+  SplashScreenDelayMs: 1 * 200
 }
