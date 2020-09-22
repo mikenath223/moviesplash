24a19701ad3643595617c996cbb5d293aca188e6
mikenath223 <michgoldennathan17@gmail.com>
2020-09-22 06:45:51 +0100

    Add method to handle get item details in Trending screen

--- app/screens/Trending/index.js
+++ app/screens/Trending/index.js
@@ -1,46 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
-import Item from 'ms/common/components/Item';
-import { API_KEY } from 'ms/common/constants';
+import React from 'react';
+import { FlatList, StyleSheet, Text } from 'react-native';
+import Item from 'ms/common/components/listItem';
+import withResultRenderer from 'ms/common/components/withResultRenderer';
+import { trendingUrl } from 'ms/common/constants/';
 
-const Trending = () => {
-  const [trends, setTrends] = useState([])
-  const [error, setError] = useState('')
-
-  useEffect(() => {
-    fetchTrends();
-  }, []);
-
-  const fetchTrends = async () => {
-    try {
-      const trendingUrl = `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}`;
-      fetch(trendingUrl).then(res => res.json())
-        .then(json => {
-          setTrends(json.results);
-        });
-    } catch (error) {
-      setError(error.message);
-      throw new Error(error);
-    }
-  }
-
+const Trending = ({ loadedData, getMoreDetails }) => {
   const _renderItem = ({ item }) => (
-    <View style={styles.padding}>
-      <Item item={item} />
-    </View>
+    <Item item={item}
+      handleGetDetails={getMoreDetails} />
   )
 
   const _keyExtractor = (item) => item.id
-
-  if (error) {
-    return (
-      <View>
-        <Text style={styles.error}>
-          {error}
-        </Text>
-      </View>
-    )
-  }
 
   return (
     <>
@@ -48,28 +18,21 @@ const Trending = () => {
         numColumns={2}
         horizontal={false}
         style={styles.flatList}
-        data={trends}
+        data={loadedData}
         keyExtractor={_keyExtractor}
         renderItem={_renderItem}
       />
       <Text>Trends</Text>
     </>
   )
-
 }
 
 const styles = StyleSheet.create(
   {
     flatList: {
-      flex: 1,
-    },
-    padding: {
-      paddingHorizontal: 5
-    },
-    error: {
-      fontSize: 20
+      marginHorizontal: 5,
     }
   }
 )
 
-export default Trending;
+export default withResultRenderer(Trending, trendingUrl);
