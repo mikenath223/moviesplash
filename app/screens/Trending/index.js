8e9d0fd12bd26126ab8276c530090b414b3d6c50
mikenath223 <michgoldennathan17@gmail.com>
2020-09-25 00:23:52 +0100

    Wrap Trending comp in HOC

--- app/screens/Trending/index.js
+++ app/screens/Trending/index.js
@@ -1,38 +1,5 @@
-import React from 'react';
-import { FlatList, StyleSheet, Text } from 'react-native';
-import Item from 'ms/common/components/listItem';
+import MediaList from 'ms/common/components/listItem';
 import withResultRenderer from 'ms/common/components/withResultRenderer';
 import { trendingUrl } from 'ms/common/constants/';
 
-const Trending = ({ loadedData, getMoreDetails }) => {
-  const _renderItem = ({ item }) => (
-    <Item item={item}
-      handleGetDetails={getMoreDetails} />
-  )
-
-  const _keyExtractor = (item) => item.id
-
-  return (
-    <>
-      <FlatList
-        numColumns={2}
-        horizontal={false}
-        style={styles.flatList}
-        data={loadedData}
-        keyExtractor={_keyExtractor}
-        renderItem={_renderItem}
-      />
-      <Text>Trends</Text>
-    </>
-  )
-}
-
-const styles = StyleSheet.create(
-  {
-    flatList: {
-      marginHorizontal: 5,
-    }
-  }
-)
-
-export default withResultRenderer(Trending, trendingUrl);
+export default withResultRenderer(MediaList, trendingUrl);
