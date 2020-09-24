a8f88c234d953b7f76c405b95af4efdff4f998bb
mikenath223 <michgoldennathan17@gmail.com>
2020-09-24 16:43:46 +0100

    Create app drawer naviagtion

--- app/App.js
+++ app/App.js
@@ -1,26 +1,30 @@
 import React, { useEffect } from 'react';
-import { Text, StatusBar, SafeAreaView, ScrollView } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import { Container } from 'moviesplash/styles/container.styled';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
+import Trending from 'ms/screens/Trending';
+import Home from 'ms/screens/Home';
+import Category from 'ms/screens/Category';
 import constants from 'ms/common/constants';
 
 const App = () => {
+  const Drawer = createDrawerNavigator();
+  const Stack = createStackNavigator();
 
   useEffect(() => {
     setTimeout(() => {
       SplashScreen.hide();
     }, constants.SplashScreenDelayMs);
-  }, [])
+  }, []);
 
   return (
     <Container>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic">
-          <Text>Hello there</Text>
-        </ScrollView>
-      </SafeAreaView>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Categories" component={Category} />
+        <Drawer.Screen name="Trending Movies/Series" component={Trending} />
+      </Drawer.Navigator>
     </Container>
   );
 };
